90f46ae6c9b0093f3efe34ebd9cedd6ca5670e73
d1rk <d1rk@gmx.de>
2013-06-07 10:45:26 +0200

    use new Engine

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
 
+use lithium\core\Libraries;
 use lithium\net\http\Media;
+
+
+// require dirname(__DIR__) . '/libraries/Mustache/Autoloader.php';
+// Mustache_Autoloader::register();
+
 
 require dirname(__DIR__) . '/libraries/mustache/Mustache.php';
 
@@ -9,10 +15,20 @@ Media::type('mustache', 'text/x-mustache', array(
 	'loader' => 'Mustache',
 	'renderer' => 'Mustache',
 	'paths' => array(
-		'template' => '{:library}/views/{:controller}/{:template}.{:type}',
-		'layout'   => '{:library}/views/layouts/{:layout}.{:type}',
-		'element'  => '{:library}/views/elements/{:template}.{:type}'
-	)
+		'template' => array(
+			LITHIUM_APP_PATH . '/views/{:controller}/{:template}.{:type}.mustache',
+			'{:library}/views/{:controller}/{:template}.{:type}.mustache',
+		),
+		'layout'   => array(
+			LITHIUM_APP_PATH . '/views/layouts/{:layout}.{:type}.mustache',
+			'{:library}/views/layouts/{:layout}.{:type}.mustache',
+		),
+		'element'  => array(
+			LITHIUM_APP_PATH . '/views/elements/{:template}.{:type}.mustache',
+			'{:library}/views/elements/{:template}.{:type}.mustache',
+		),
+	),
+	'cache' => LITHIUM_APP_PATH . '/resources/tmp/cache/mustache',
 ));
 
 ?>
--- extensions/adapter/template/view/Mustache.php
+++ extensions/adapter/template/view/Mustache.php
@@ -2,16 +2,28 @@
 
 namespace li3_mustache\extensions\adapter\template\view;
 
-use li3_mustache\libraries\Mustache as Renderer;
+use Mustache_Engine;
 use lithium\core\Libraries;
 
 class Mustache extends \lithium\template\view\adapter\File {
 
+	/**
+	 * `Mustache_Engine` object instance used by this adapter.
+	 *
+	 * @var object
+	 */
+	private $_engine = null;
+
+	public function _init() {
+		parent::_init();
+		$this->_engine = new Mustache_Engine($this->_config);
+	}
+
 	public function render($template, $data = array(), array $options = array()) {
 		$this->_context = $options['context'] + $this->_context;
+		$this->_engine->setHelpers($this->_context);
 		$this->_data = (array) $data + $this->_vars;
-		$renderer = new Renderer();
-		return $renderer->render(file_get_contents($template), $this->_context + $this->_data);
+		return $this->_engine->render(file_get_contents($template), $this->_data);
 	}
 
 	/**
@@ -22,8 +34,8 @@ class Mustache extends \lithium\template\view\adapter\File {
 	 * @return string
 	 */
 	public function template($type, array $params) {
-		$params += array('library' => true);
-		$params['library'] = Libraries::get($params['library'], 'path');
+		$library = Libraries::get(isset($params['library']) ? $params['library'] : true);
+		$params['library'] = $library['path'];
 		return $this->_paths($type, $params);
 	}
 }
